d9a26bdf301f3362a1c9e387b66f71cc9ecda308
mson90906250 <l90906250@gmail.com>
2019-02-15 16:24:25 +0800

    新增navgation 和 製作重複區域 上傳一些東西

--- index.php
+++ index.php
@@ -1,12 +1,101 @@
-<?php require_once('Connections/forum.php'); ?>
+<?php
+#	BuildNav for Dreamweaver MX v0.2
+#              10-02-2002
+#	Alessandro Crugnola [TMM]
+#	sephiroth: alessandro@sephiroth.it
+#	http://www.sephiroth.it
+#	
+#	Function for navigation build ::
+function buildNavigation($pageNum_Recordset1,$totalPages_Recordset1,$prev_Recordset1,$next_Recordset1,$separator=" | ",$max_links=10, $show_page=true)
+{
+                GLOBAL $maxRows_Recordset1,$totalRows_Recordset1;
+	$pagesArray = ""; $firstArray = ""; $lastArray = "";
+	if($max_links<2)$max_links=2;
+	if($pageNum_Recordset1<=$totalPages_Recordset1 && $pageNum_Recordset1>=0)
+	{
+		if ($pageNum_Recordset1 > ceil($max_links/2))
+		{
+			$fgp = $pageNum_Recordset1 - ceil($max_links/2) > 0 ? $pageNum_Recordset1 - ceil($max_links/2) : 1;
+			$egp = $pageNum_Recordset1 + ceil($max_links/2);
+			if ($egp >= $totalPages_Recordset1)
+			{
+				$egp = $totalPages_Recordset1+1;
+				$fgp = $totalPages_Recordset1 - ($max_links-1) > 0 ? $totalPages_Recordset1  - ($max_links-1) : 1;
+			}
+		}
+		else {
+			$fgp = 0;
+			$egp = $totalPages_Recordset1 >= $max_links ? $max_links : $totalPages_Recordset1+1;
+		}
+		if($totalPages_Recordset1 >= 1) {
+			#	------------------------
+			#	Searching for $_GET vars
+			#	------------------------
+			$_get_vars = '';			
+			if(!empty($_GET) || !empty($HTTP_GET_VARS)){
+				$_GET = empty($_GET) ? $HTTP_GET_VARS : $_GET;
+				foreach ($_GET as $_get_name => $_get_value) {
+					if ($_get_name != "pageNum_Recordset1") {
+						$_get_vars .= "&$_get_name=$_get_value";
+					}
+				}
+			}
+			$successivo = $pageNum_Recordset1+1;
+			$precedente = $pageNum_Recordset1-1;
+			$firstArray = ($pageNum_Recordset1 > 0) ? "<a href=\"$_SERVER[PHP_SELF]?pageNum_Recordset1=$precedente$_get_vars\">$prev_Recordset1</a>" :  "$prev_Recordset1";
+			# ----------------------
+			# page numbers
+			# ----------------------
+			for($a = $fgp+1; $a <= $egp; $a++){
+				$theNext = $a-1;
+				if($show_page)
+				{
+					$textLink = $a;
+				} else {
+					$min_l = (($a-1)*$maxRows_Recordset1) + 1;
+					$max_l = ($a*$maxRows_Recordset1 >= $totalRows_Recordset1) ? $totalRows_Recordset1 : ($a*$maxRows_Recordset1);
+					$textLink = "$min_l - $max_l";
+				}
+				$_ss_k = floor($theNext/26);
+				if ($theNext != $pageNum_Recordset1)
+				{
+					$pagesArray .= "<a href=\"$_SERVER[PHP_SELF]?pageNum_Recordset1=$theNext$_get_vars\">";
+					$pagesArray .= "$textLink</a>" . ($theNext < $egp-1 ? $separator : "");
+				} else {
+					$pagesArray .= "$textLink"  . ($theNext < $egp-1 ? $separator : "");
+				}
+			}
+			$theNext = $pageNum_Recordset1+1;
+			$offset_end = $totalPages_Recordset1;
+			$lastArray = ($pageNum_Recordset1 < $totalPages_Recordset1) ? "<a href=\"$_SERVER[PHP_SELF]?pageNum_Recordset1=$successivo$_get_vars\">$next_Recordset1</a>" : "$next_Recordset1";
+		}
+	}
+	return array($firstArray,$pagesArray,$lastArray);
+}
+?><?php require_once('Connections/forum.php'); ?>
 
 <?php
 //繫結資料集
+$maxRows_Recordset1 = 3;
+$pageNum_Recordset1 = 0;
+if (isset($_GET['pageNum_Recordset1'])) {
+  $pageNum_Recordset1 = $_GET['pageNum_Recordset1'];
+}
+$startRow_Recordset1 = $pageNum_Recordset1 * $maxRows_Recordset1;
+
 mysql_select_db($database_forum, $forum);
 $query_Recordset1 = "SELECT * FROM topic ORDER BY TopicID DESC";
-$Recordset1 = mysql_query($query_Recordset1, $forum) or die(mysql_error());
+$query_limit_Recordset1 = sprintf("%s LIMIT %d, %d", $query_Recordset1, $startRow_Recordset1, $maxRows_Recordset1);
+$Recordset1 = mysql_query($query_limit_Recordset1, $forum) or die(mysql_error());
 $row_Recordset1 = mysql_fetch_assoc($Recordset1);
-$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+
+if (isset($_GET['totalRows_Recordset1'])) {
+  $totalRows_Recordset1 = $_GET['totalRows_Recordset1'];
+} else {
+  $all_Recordset1 = mysql_query($query_Recordset1);
+  $totalRows_Recordset1 = mysql_num_rows($all_Recordset1);
+}
+$totalPages_Recordset1 = ceil($totalRows_Recordset1/$maxRows_Recordset1)-1;
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -31,16 +120,32 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
     <td>發表人</td>
     <td>時間</td>
   </tr>
-  <tr>
-    <td><?php echo $row_Recordset1['Title']; ?></td>
-    <td><?php echo $row_Recordset1['Nickname']; ?></td>
-    <td><?php echo $row_Recordset1['Time']; ?></td>
-  </tr>
+  
+  <?php //製作重複區域 
+   do { ?>
+    <tr>
+      <td><?php echo $row_Recordset1['Title']; ?></td>
+      <td><?php echo $row_Recordset1['Nickname']; ?></td>
+      <td><?php echo $row_Recordset1['Time']; ?></td>
+    </tr>
+    <?php } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1)); ?>
 </table>
 
 <table width="500" border="1" align="center">
   <tr>
-    <td><div align="right">跳頁選單</div></td>
+    <td><div align="right">
+      <?php 
+	  //navigation builder
+# variable declaration
+$prev_Recordset1 = "« previous";
+$next_Recordset1 = "next »";
+$separator = " | ";
+$max_links = 10;
+$pages_navigation_Recordset1 = buildNavigation($pageNum_Recordset1,$totalPages_Recordset1,$prev_Recordset1,$next_Recordset1,$separator,$max_links,true); 
+
+print $pages_navigation_Recordset1[0]; 
+?>
+      <?php print $pages_navigation_Recordset1[1]; ?> <?php print $pages_navigation_Recordset1[2]; ?></div></td>
   </tr>
 </table>
 
